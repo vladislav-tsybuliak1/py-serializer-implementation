0b1e789b986e7c769109a676b2dac4d8673e3228
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-26 18:57:46 +0300

    Add serializer and functions in main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return Car.objects.create(**serializer.data)
+    return serializer.errors
